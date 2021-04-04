51a42263996a1b2bfd58e10e6736f2be3232fc93
GuhTiagoSilva <46786490+GuhTiagoSilva@users.noreply.github.com>
2021-04-03 21:29:59 -0300

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
         home: Scaffold(
-      body: Column(
-        children: [
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text("100.0"),
-              subtitle: Text("1000"),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text("200.0"),
-              subtitle: Text("1000"),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text("300.0"),
-              subtitle: Text("1000"),
-            ),
-          ),
-        ],
-      ),
+      body: TransferList(),
       appBar: AppBar(
         title: Text("Transferências"),
       ),
@@ -34,3 +10,39 @@ void main() => runApp(MaterialApp(
         child: Icon(Icons.add),
       ),
     )));
+
+
+class TransferList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransferItem(Transfer(100.0, 1000)),
+        TransferItem(Transfer(200.0, 1001)),
+        TransferItem(Transfer(400.0, 1002)),
+      ],
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget{
+  final Transfer _transfer;
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer.value.toString()),
+        subtitle: Text(_transfer.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer{
+  final double value;
+  final int accountNumber;
+  Transfer(this.value, this.accountNumber);
+}
